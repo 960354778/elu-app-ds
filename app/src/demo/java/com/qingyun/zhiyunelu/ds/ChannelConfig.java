86be2ddd81fdb2efc903de7ff11635f584c3c997
Regis WANG <regis.wang@zhiyunmed.com>
2018-03-01 23:12:37 +0800

    added channel: demo

---
+++ app/src/demo/java/com/qingyun/zhiyunelu/ds/ChannelConfig.java
@@ -0,0 +1,12 @@
+package com.qingyun.zhiyunelu.ds;
+
+/**
+ * Created by regis on 17/4/25.
+ */
+
+public final class ChannelConfig {
+    private ChannelConfig() {}
+
+    public static final boolean FORCE_SHOW_LOG = true;
+    public static final String BASE_URL = "http://api.demo.zhiyunyilu.com/";
+}
