0e32b9e0ec3b6a14683a96f67b503bc4bab54250
Regis WANG <regis.wang@zhiyunmed.com>
2018-10-07 02:44:24 +0800

    adjust config

--- app/src/qa/java/com/qingyun/zhiyunelu/ds/ChannelConfig.java
+++ app/src/qa/java/com/qingyun/zhiyunelu/ds/ChannelConfig.java
@@ -1,5 +1,7 @@
 package com.qingyun.zhiyunelu.ds;
 
+import com.qingyun.zhiyunelu.ds.data.Setting;
+
 /**
  * Created by regis on 17/4/25.
  */
@@ -7,6 +9,16 @@ package com.qingyun.zhiyunelu.ds;
 public final class ChannelConfig {
     private ChannelConfig() {}
 
-    public static final boolean FORCE_SHOW_LOG = true;
-    public static final String BASE_URL = "https://api.qa.zhiyunyilu.com/";
+    public static final Setting SETTING_CHANNEL;
+
+    private static Setting buildChannelSetting() {
+        Setting ret = new Setting();
+        ret.network = new Setting.Network();
+        ret.network.apiRootUrl = "https://api.qa.zhiyunyilu.com/";
+        return ret;
+    }
+
+    static {
+        SETTING_CHANNEL = buildChannelSetting();
+    }
 }
