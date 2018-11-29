10be3c37d9db84d8a9f6f3ad2ed2278b0111af53
Regis WANG <regis.wang@zhiyunmed.com>
2018-11-29 14:23:08 +0800

    add new channel tra

---
+++ app/src/tra/java/com/qingyun/zhiyunelu/ds/ChannelConfig.java
@@ -0,0 +1,24 @@
+package com.qingyun.zhiyunelu.ds;
+
+import com.qingyun.zhiyunelu.ds.data.Setting;
+
+/**
+ * Created by regis on 17/4/25.
+ */
+
+public final class ChannelConfig {
+    private ChannelConfig() {}
+
+    public static final Setting SETTING_CHANNEL;
+
+    private static Setting buildChannelSetting() {
+        Setting ret = new Setting();
+        ret.network = new Setting.Network();
+        ret.network.apiRootUrl = "https://tra-api.zhiyunyilu.com/";
+        return ret;
+    }
+
+    static {
+        SETTING_CHANNEL = buildChannelSetting();
+    }
+}
